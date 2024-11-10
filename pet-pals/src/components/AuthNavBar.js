d048e2e585ca237dbff19bd8c40e3b878d6c6019
fion-lei <137603599+fion-lei@users.noreply.github.com>
2024-11-10 03:17:49 -0700

    GIF GIF GIF

--- pet-pals/src/components/AuthNavBar.js
+++ pet-pals/src/components/AuthNavBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Group, Button, Center, Image, Container } from '@mantine/core';
-import LogoImage from '../assets/Images/logo.png';
+import LogoImage from '../assets/Images/logo_gif.gif';
 
 const AuthNavBar = ({ method, toggleMethod }) => {
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid #e0e0e0' }}>
       <Container>
         <Center>
-          <Image src={LogoImage} alt="Logo" h={80} w={80} />
+          <Image src={LogoImage} alt="Logo" h={200} w={200} />
         </Center>
         <Center>
           <Group spacing="sm" position="center" mt="md">
