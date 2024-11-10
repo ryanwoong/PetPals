66404edfe72a8af17bc812367abc98e2b4f87502
ryan wong <ryanpersonal18@gmail.com>
2024-11-09 18:30:03 -0700

    Fix

--- pet-pals/src/pages/HomePage.js
+++ pet-pals/src/pages/HomePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from "@mantine/core";
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
@@ -9,6 +11,7 @@ const HomePage = () => {
       <main>
         <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
         <img src="https://via.placeholder.com/300" alt="Placeholder" />
+        <Button><Link to="/register">Register</Link></Button>
       </main>
     </div>
   );
